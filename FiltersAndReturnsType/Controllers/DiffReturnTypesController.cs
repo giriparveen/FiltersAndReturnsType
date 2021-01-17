02d7e762d51a5aecac5cd125a7493e143bc7f896
giriparveen <giriparveen@gmail.com>
2021-01-17 19:39:14 +0530

    Added different return type in action method

---
+++ FiltersAndReturnsType/Controllers/DiffReturnTypesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiltersAndReturnsType.Controllers
+{
+    public class DiffReturnTypesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public ViewResult GetDiffReturnType()
+        //{
+        //    ViewBag.Type = "ViewResult";
+        //    return View();
+        //}
+        //public IActionResult GetDiffReturnType()
+        //{
+        //    ViewBag.Type = "IActionResult";
+        //    return View();
+        //}
+        //public RedirectToActionResult GetDiffReturnType()
+        //{
+        //    ViewBag.Type = "RedirectToActionResult";
+        //    return RedirectToAction("index");
+        //}
+        //public EmptyResult GetDiffReturnType()
+        //{
+        //    ViewBag.Type = "EmptyResult";
+        //    return new EmptyResult();
+        //}
+        //public ContentResult GetDiffReturnType()
+        //{
+        //    ViewBag.Type = "ContentResult";
+        //    return Content("I am from ContentResult");
+        //}
+        public JsonResult GetDiffReturnType()
+        {
+            ViewBag.Type = "JsonResult";
+            return Json("Json Result Data");
+        }
+    }
+}
